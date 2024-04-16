1056b0634dfaa60f44bedc86b68e3e725f62b50a
CassianoE <kssycs@outlook.com>
2024-04-16 06:51:43 -0300

    initial commit

--- src/Tabuada.java
+++ src/Tabuada.java
@@ -14,6 +14,7 @@ public class Tabuada {
             for (i = 0; i <= 10; i++) {
                 System.out.println(valor + " x " + i + " = " + (valor * i));
             }
+
         teclado.close();
     }
 }
